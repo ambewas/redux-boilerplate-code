0da3ab982f95613ac89eee8dab4eb90a638a5198
Kevin Decock <kevin.decock@district01.be>
2018-06-25 14:58:37 +0200

    user ref removed

--- redux/store/reducers.js
+++ redux/store/reducers.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
-import userReducer from "../user/userReducer";
 import asyncExampleReducer from "../asyncExample/asyncExampleReducer";
 
 const app = combineReducers({
-	user: userReducer,
 	asyncExample: asyncExampleReducer,
 });
 
